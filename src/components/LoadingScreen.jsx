f83b9078bbef53ac1994803b60031f5295dd0d0a
Jamesdakoano <lebronjamesdelacruz89@gmail.com>
2025-03-31 15:45:22 +0800

    third changes

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -20,7 +20,7 @@ export const LoadingScreen = ({ onComplete }) => {
         <div className="absolute inset-0 rounded-full border-4 border-t-blue-500 border-r-transparent border-b-blue-500 border-l-transparent animate-spin"></div>
         
         <img
-          src="https://scontent.fceb8-1.fna.fbcdn.net/v/t39.30808-6/465918685_2853847418125933_656620028390066708_n.jpg?stp=dst-jpg_s552x414_tt6&_nc_cat=100&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeELSS5HipNHFPQmLjFWh6qKxrJRmAURCafGslGYBREJp_t-vRyxSJjwsO3D5CMy86S4iDIdnOgQPh7o-z-g1q_J&_nc_ohc=11jgIlSQBwUQ7kNvgFBFZMG&_nc_oc=AdntHIddRMhE3gBzPlOGlJvlQ8d9_3kFSU2Cu9kXbg8Ft9R0wlOLjYNBrISqsaACvoBYs99mj0XXHw4B0WKh4-GI&_nc_zt=23&_nc_ht=scontent.fceb8-1.fna&_nc_gid=dMMrtympZpLZcTdo_nigCQ&oh=00_AYEgMg7qZkX7zYM8T9nkX6TrdTwVsdl_nF8HAAu4hU0QtA&oe=67EEAF66"
+          src="https://scontent.filo1-1.fna.fbcdn.net/v/t1.15752-9/486028092_1117515830131271_7495457875870986086_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=111&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeFcPf1RvN5Yq__OKfY0O6SbZ8q7QXHCIdVnyrtBccIh1cqCzDPUrtwKFOxy9YcKn3c3SdPK0TyH1mK2jxNgzO72&_nc_ohc=xPrn5JivOLEQ7kNvgExQ9SG&_nc_oc=Adm_G_vhivPD_Ht1SPijiPyGo29I7t29s6PhJFANnj1ZwBuH8BNX8W4gWPfqfB14J3I&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.filo1-1.fna&oh=03_Q7cD1wGWf1LQFfnVISVkteLcXT7irqks-2N36EIN3y3tS6fCCQ&oe=6811A275"
           alt="James Pico"
           className="w-full h-full rounded-full border-4 border-gray-800 p-2"
         />
